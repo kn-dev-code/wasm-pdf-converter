2867587d3578a21419ba8352d7ea893bbaeb14ae
Kyle <nettervillekyle@gmail.com>
2026-03-24 13:53:08 -0500

    Completed ui layout of dashboard and navbar tsx

--- source/frontend/wasm-engine/src/components/dashboard.tsx
+++ source/frontend/wasm-engine/src/components/dashboard.tsx
@@ -1,47 +1,67 @@
 import { Card, CardDescription, CardTitle } from "./ui/card";
-
+import combinePdf from "../assets/combine-image.png";
+import splitPdf from "../assets/split-image.png";
+import compressPdf from "../assets/compress-image.png";
+import mergePdf from "../assets/merge-image.png";
+import toPdf from "../assets/to-pdf-image.png"
+import { Link } from "react-router-dom";
 const Dashboard = () => {
   const cardLayout = [
     {
       id: "merge",
+      image: mergePdf,
+      linkName: "/convert/merge",
       title: "MERGE PDF",
       description: "Merge 1-2 PDFs with an order that suits you.",
     },
     {
       id: "combine",
+      image: combinePdf,
+      linkName: "/convert/combine",
       title: "COMBINE PDF",
       description: "Combine more than 2 PDFS with an order that suits you.",
     },
     {
       id: "compress",
+      image: compressPdf,
+      linkName: "/convert/compress",
       title: "COMPRESS PDF",
       description: "Reduce the file-size while ensuring high quality format.",
     },
     {
       id: "split",
+      image: splitPdf,
+      linkName: "/convert/split",
       title: "SPLIT PDF",
-      description: "Set PDFs in a seperate compartment of its own file.",
+      description: "Set PDFs in seperate files.",
     },
     {
       id: "to-pdf",
+      image: toPdf,
+      linkName: "/convert/to-pdf",
       title: "TO-PDF",
       description: "Convert any file-type into PDF format.",
     },
   ];
   return (
-    <div className="bg-[#505252] h-screen">
+    <div className="bg-[#505252] min-h-screen">
       <p className="flex justify-center items-center text-4xl text-white pt-10">
         No Tracking. Simple and private conversion in one place.
       </p>
-      <div></div>
-      {cardLayout.map((card) => (
-        <div key={card.id} className="grid grid-cols-1 md:grid-cols-6 ">
-          <Card className = "p-12 w-60">
-            <CardTitle>{card.title}</CardTitle>
-            <CardDescription>{card.description}</CardDescription>
+      <div className="grid grid-cols-1 md:grid-cols-6 pl-35 pt-20 gap-10 max-w-6xl mx-auto px-20">
+      {cardLayout.map((card,index) => {
+        const isBottomIndex = index >=3;
+
+
+        return (
+          <Card key = {card.id} className = {`flex flex-col items-center p-5 w-60 h-45 bg-[#848484] rounded-2xl ${isBottomIndex && index === 3 ? "md:col-start-2" : ""} col-span-1 md:col-span-2`}>
+            <Link className = "w-15 h-15 mr-55 -translate-y-8 absolute hover:scale-105 duration-75 transition-all" to = {card.linkName}><img alt = {card.title} className = ""src = {card.image}/></Link>
+            <CardTitle className = "text-black">{card.title}</CardTitle>
+            <CardDescription className = "text-white font-bold pl-5">{card.description}</CardDescription>
           </Card>
-        </div>
-      ))}
+        )
+      })}
+      </div>
     </div>
   );
 };
--- source/frontend/wasm-engine/src/components/navbar.tsx
+++ source/frontend/wasm-engine/src/components/navbar.tsx
@@ -1,28 +1,29 @@
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
-
+import navBar from "../assets/nav-image.png";
 const NavBar = () => {
   return (
     <nav>
-      <div className="flex flex-row justify-around gap-15 h-15 bg-[#8F8B8B] items-center px-20 pl-30 border-2 border-white">
-        <h1 className="text-white font-bold text-3xl">BlinkFlow</h1>
-        <Link className="text-white font-bold text-[17px]" to="/convert/merge">
+      <div className="flex flex-row justify-around gap-10 w-screen h-15 bg-[#8F8B8B] items-center pl-20 border-3 border-white">
+        <img className = "w-10 h-10 relative right-20" src = {navBar}/>
+        <Link className="text-white font-bold text-3xl relative right-28 " to= "/">BlinkFlow</Link>
+        <Link className="text-white font-bold text-[17px] relative right-10 hover:text-[#e7e2e2] cursor-pointer" to="/convert/merge">
           MERGE PDF
         </Link>
-        <Link className="text-white font-bold text-[17px]" to="/convert/combine">
+        <Link className="text-white font-bold text-[17px] relative right-10 hover:text-[#e7e2e2] cursor-pointer" to="/convert/combine">
           COMBINE PDF
         </Link>
-        <Link className="text-white font-bold text-[17px]" to="/convert/compress">
+        <Link className="text-white font-bold text-[17px] relative right-10 hover:text-[#e7e2e2] cursor-pointer" to="/convert/compress">
           COMPRESS PDF
         </Link>
-        <Link className="text-white font-bold text-[17px]" to="/convert/split">
+        <Link className="text-white font-bold text-[17px] relative right-10 hover:text-[#e7e2e2] cursor-pointer" to="/convert/split">
           SPLIT PDF
         </Link>
-        <Link className="text-white font-bold text-[17px]" to="/convert/to-pdf">
+        <Link className="text-white font-bold text-[17px] relative right-10 hover:text-[#e7e2e2] cursor-pointer" to="/convert/to-pdf">
           TO-PDF
         </Link>
-        <Button className="bg-[#575555] font-bold p-5 text-sm relative left-13">Sign Up</Button>
-        <Button className="bg-[#575555] font-bold p-5 text-sm">Log In</Button>
+        <Link to = "/sign-up"><Button className="bg-[#575555] font-bold p-5 text-sm relative left-5 hover:scale-105 transition-all duration-300 cursor-pointer">Sign Up</Button></Link>
+        <Link to = "/log-in"><Button className="bg-[#575555] font-bold p-5 text-sm relative right-5 hover:scale-105 transition-all duration-300 cursor-pointer">Log In</Button></Link>
       </div>
     </nav>
   );
